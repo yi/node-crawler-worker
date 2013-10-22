b1ba7bea60cb207a94f16867f3eabc5604e55974
yi <yi2004@gmail.com>
2013-10-22 16:30:42 +0800

    refind scripts

--- forever-start.sample.sh
+++ forever-start.sample.sh
@@ -4,7 +4,6 @@
 # Get absolute path of script
 pushd `dirname $0` > /dev/null
 GS_PATH=`pwd`
-CONF_PATH=$GS_PATH/gs.js
 popd > /dev/null
 
 GS_SERVER_ID=2
@@ -12,11 +11,10 @@ CROWLER_SERVER_REDIS_HOST=localhost
 CROWLER_SERVER_REDIS_PORT=6379
 PATH_OUTPUT_WEB_PAGES=/tmp/crawl-output
 
-mkdir -p ~/log
+mkdir -p $GS_PATH/log
 mkdir -p $PATH_OUTPUT_WEB_PAGES
 touch $GS_PATH/log/crawler-worker.log
 forever start -a -l $GS_PATH/log/crawler-worker.log --pidFile $GS_PATH/crawler-worker.pid $GS_PATH/lib/crawler-worker.js -g md -o $PATH_OUTPUT_WEB_PAGES
-#forever start -a -l $GS_PATH/log/forever.log -o $GS_PATH/log/crawler-worker.log -e $GS_PATH/log/crawler-worker.log --pidFile $GS_PATH/crawler-worker.pid $GS_PATH/lib/crawler-worker.js -g md -o $PATH_OUTPUT_WEB_PAGES
 echo "Forever start: crawler-worker --- DONE."
 
 
--- start-md15.sh
+++ start-md15.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p log
+mkdir -p output
 kill `cat ./log/crawler-worker.pid`
 node lib/crawler-worker.js -g "md15" -p 6379 -h localhost -o output/ > log/crawler-worker.log 2>&1 &
 echo $!>./log/crawler-worker.pid
